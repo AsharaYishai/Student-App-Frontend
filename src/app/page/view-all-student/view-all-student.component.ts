64b97fa08aa956af2ff8c80247f1523fc8118577
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-24 15:24:52 +0530

    delete function

--- src/app/page/view-all-student/view-all-student.component.ts
+++ src/app/page/view-all-student/view-all-student.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-student',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule],
+  imports: [HttpClientModule,FormsModule,CommonModule,],
   templateUrl: './view-all-student.component.html',
   styleUrl: './view-all-student.component.css'
 })
@@ -16,12 +17,56 @@ export class ViewAllStudentComponent {
 
   constructor(private http:HttpClient){
     this.loadStudentTable();
+    
   }
   loadStudentTable(){
     this.http.get("http://localhost:8080/student-controller/get-all").subscribe(res=>{
       this.studentList=res;
       console.log(res);
-      
     })
+  }
+
+  deleteStudent(student:any){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) { 
+        this.http.delete(`http://localhost:8080/student-controller/delete-student/${student.id}`,{responseType:'text'}).subscribe(res=>{
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+          this.loadStudentTable()
+          console.log(res);
+        })
+        console.log(student);
+
+    
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
+
   }
 }
