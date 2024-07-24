2cc76446f683fc1958d407621f11039ae473bc2e
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-24 11:44:38 +0530

    call Add API

--- src/app/page/manage-student/manage-student.component.ts
+++ src/app/page/manage-student/manage-student.component.ts
@@ -1,18 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-student',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './manage-student.component.html',
   styleUrl: './manage-student.component.css'
 })
 export class ManageStudentComponent {
 
-   public firstName:String=""
+  public studentObj = {
+    firstName:"",
+    lastName:"",
+    dob:"",
+    address:"",
+    city:"",
+    religion:"",
+    gender:"",
+    email:"",
+    phoneNumber:"",
+    postalCode:"",
+    zipCode:"",
+  }
+
+  constructor(private http:HttpClient){
+
+  }
 
   addStudent(){
-    alert(this.firstName)
+    this.http.post("http://localhost:8080/student-controller/add-student",this.studentObj).subscribe(
+      (data) =>{
+        console.log(data);
+        
+      }
+    )
+    
   }
 }
