aacfde4de665f0c159e34f3d877a09c76ef4e6ff
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-24 10:53:56 +0530

    ng Model

--- src/app/page/manage-student/manage-student.component.ts
+++ src/app/page/manage-student/manage-student.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-student',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './manage-student.component.html',
   styleUrl: './manage-student.component.css'
 })
 export class ManageStudentComponent {
 
+   public firstName:String=""
+
+  addStudent(){
+    alert(this.firstName)
+  }
 }
